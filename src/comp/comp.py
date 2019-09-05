a2739b64d8eead6b5766b456eb35b0101197551e
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-09-04 23:15:11 -0400

    added list comprehension for Names and ages between 27 and 32

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -54,7 +54,7 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [f'{Human.name}, {Human.age}' for Human in humans if Human.age >= 27 and Human.age <= 32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
