f12ba2234b302872bc0329a891872ea037c903af
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-12-02 21:32:39 -0500

    minor fixes to g and h

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -62,11 +62,11 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [Human.name.upper(), (Human.age + 5) for Human in humans]
+g = [(Human.name.upper(), Human.age + 5) for Human in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = [sqrt(Human.age) for Human in humans]
+h = [math.sqrt(Human.age) for Human in humans]
 print(h)
